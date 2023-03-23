fa8cf3f0815c3554b3b8c2e1b83100e639da61c4
Juan Vallecilla <juanmanuelvallecilla@hotmail.ca>
2023-03-23 12:42:01 -0400

    grouping things

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,32 +9,34 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+const group = new THREE.Group();
+group.scale.y = 0.2;
+scene.add(group);
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
+cube1.position.x = -2;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
-
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
-
-// Rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
-
-scene.add(mesh);
 
 /**
  * Sizes
@@ -49,9 +51,6 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-
-// LookAt
-camera.lookAt(mesh.position);
 
 scene.add(camera);
 
