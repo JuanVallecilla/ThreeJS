e41b34bc5f1371b977aee5794316ddb8cf140c58
Juan Vallecilla <juanmanuelvallecilla@hotmail.ca>
2023-05-29 06:00:28 -0400

    adding fog

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -14,6 +14,9 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Fog
+const fog = new THREE.Fog('#262837', 1, 15)
+scene.fog = fog
 /**
  * Textures
  */
@@ -176,6 +179,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837')
 
 /**
  * Animate
